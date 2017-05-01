482d1fabf3a85dc84537b284d14255c2a52d66f9
Lucas Neves <lcneves@gmail.com>
2017-05-01 23:39:35 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+/*
+ * index.js
+ * Copyright 2017 Lucas Neves <lcneves@gmail.com>
+ *
+ * Entry point for the Livre3D engine.
+ * Initializes the Livre client with the engine contained in this directory.
+ *
+ */
+
+'use strict';
+
+module.exports = function (options) {
+
+  // These options were passed by the theme.
+  const engine = require('./engine.js')(options);
+
+  require('livre-client')({
+    engine: engine
+  });
+
